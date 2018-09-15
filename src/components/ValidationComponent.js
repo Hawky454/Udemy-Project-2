8bb69d9772b002c4a938e2b0f5570e6a2d380c16
David Miller <hawky454@gmail.com>
2018-09-15 00:01:42 -0500

    finally got my if else statements to properly execute when the appropriate numbers were reached

--- src/components/ValidationComponent.js
+++ src/components/ValidationComponent.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 const ValidationComponent = (props) => {
-  if(props.length < 10) {
+  if(props.length <= 50) {
     return(
       <div>
         <p style={{color: 'red'}}>Text is too short!</p>
         <p>{props.length}</p>
       </div>
     );
-  } else if(props.length > 50) {
+  } else if(props.length > 50 && props.length <= 199) {
     return(
       <div>
         <h2 style={{color: 'green'}}>Text is long enough!</h2>
         <p>{props.length}</p>
       </div>
     );
-  } else if(props.length > 100) {
+  }else if(props.length >= 200) {
     return(
       <div>
         <h1 style={{color: 'red'}}>Enough already, that is way too long!</h1>
